91290a21ffab940b26f32dd88714b7ae1984d4a7
Shawn Lim <shawnlim.ws@gmail.com>
2016-01-26 11:28:45 +0800

    magic 8 ball

---
+++ games_on_rails/app/controllers/magic_ball_controller.rb
@@ -0,0 +1,19 @@
+class MagicBallController < ApplicationController
+
+  def result
+    @magic_answer = generate_random_response
+    p generate_random_response
+    p @magic_answer
+  end
+
+  def generate_random_response
+    all_responses = [
+      "LGTM",
+      "SGTM",
+      "I hope this works",
+      "Oh! Its working!",
+      "Chum liao chum liao"
+    ]
+    all_responses.sample # selects a random answer
+  end
+end
